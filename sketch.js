a56afa26fd922bc41fe8db88f827dbb9c1118912
za7aDotexe <rabiezaha@gmail.com>
2020-09-24 16:05:40 -0100

    drawin 3x3 table !

--- sketch.js
+++ sketch.js
@@ -1,24 +1,22 @@
+let board = [
+    ['x','o','x'],
+    ['x','o','x'],
+    ['x','o','x']
+];
+
+let player1 = "X";
+let player2 = "O";
+
 function setup() {
     // Create the canvas
-    createCanvas(720, 400);
-    background(200);
-  
-    // Set colors
-    fill(204, 101, 192, 127);
-    stroke(127, 63, 120);
-  
-    // A rectangle
-    rect(40, 120, 120, 40);
-    // An ellipse
-    ellipse(240, 240, 80, 80);
-    // A triangle
-    triangle(300, 100, 320, 100, 310, 80);
-  
-    // A design for a simple flower
-    translate(580, 200);
-    noStroke();
-    for (let i = 0; i < 10; i ++) {
-      ellipse(0, 30, 20, 80);
-      rotate(PI/5);
-    }
+    createCanvas(400, 400);
   }
+
+function draw() {
+    background(220);
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+
+        }
+    }
+}
