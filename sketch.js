1343cd4561533a2db50d79bee9c3db945f2ca122
za7aDotexe <rabiezaha@gmail.com>
2020-09-25 00:17:39 -0100

    making some progress with js pfffffffff

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 let board = [
-    ['x','o','x'],
-    ['x','o','x'],
-    ['x','o','x']
-];
+    ['','',''],
+    ['','',''],
+    ['','','']
+]
 
-let player1 = "X";
-let player2 = "O";
+let players = ['X','O'];
+
+let currentPlayer;
+function setup() {
+    createCanvas(400,400);
+    currentPlayer = random(players);
+}
 
 function setup() {
     // Create the canvas
@@ -14,5 +19,28 @@ function setup() {
 
 function draw() {
     background(220);
+    let w = width / 3;
+    let h = height / 3;
 
+    line(w, 0, w, height);
+    line(w * 2, 0, w * 2, height);
+    line(0, h, width, h);
+    line(0, h * 2, width, h * 2);
+
+    for (let j = 0; j < 3; j++) {
+        for (let i = 0; i < 3; i++) {
+            let x = w * i + w/2;
+            let y = h * j + h/2;
+            let spot = board[i][j];
+            strokeWeight(4);
+            if (spot == players[1]) {
+                noFill();
+                ellipse(x,y,w/2);
+            }else if (spot == players[0]) {
+                let xr = w/4;
+                line(x-xr, y-xr, x +xr, y +xr);
+                line(x +xr, y-xr, x-xr, y +xr);
+            }
+        }
+    }
 }
